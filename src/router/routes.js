4c68332bf7b8f1b9c2e7e6f6069f41dbcbdac937
silence <616339210@qq.com>
2022-06-29 10:17:08 +0800

    更新

---
+++ src/router/routes.js
@@ -0,0 +1,12 @@
+
+import HelloWorld from "@/components/HelloWorld";
+ 
+var routes = [
+    {
+        path:"/",
+        name:"HelloWorld",
+        component:HelloWorld
+    }
+]
+ 
+export default routes
